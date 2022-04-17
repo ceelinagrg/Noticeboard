51c565867d2ef356c31d75e23080119bd1a87083
Salina Gurung <gsalina950@gmail.com>
2022-04-17 23:15:59 +0545

    add permission to admin

--- connect_app/permissions.py
+++ connect_app/permissions.py
@@ -1,37 +1,13 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    edit_methods = ("PUT, PATCH")
     
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
+        if request.user.is_superuser and request.method not in self.edit_methods:
             return True
 
         if request.method in permissions.SAFE_METHODS:
             return True
             
         return obj.owner == request.user
-
-        
-
-# class IsOwnerOrReadOnly(permissions.BasePermission):
-
-#     edit_methods = ("DELETE")
-
-#     def has_permission(self, request, view):
-#         if request.user.is_authenticated:
-#             return True
-
-#     def has_object_permission(self, request, view, obj):
-#         if request.user.is_superuser:
-#             return True
-
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-
-#         if obj.owner == request.user:
-#             return True
-
-#         if request.user.is_staff and request.method not in self.edit_methods:
-#             return True
-
-#         return False
